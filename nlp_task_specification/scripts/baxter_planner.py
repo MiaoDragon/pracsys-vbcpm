94fc09c6f2c1afb732263de0bf5bc7768a5c2013
Daniel Nakhimovich <dnahimov@gmail.com>
2022-02-14 01:28:20 +0200

    works but slips

---
+++ nlp_task_specification/scripts/baxter_planner.py
@@ -0,0 +1,234 @@
+import sys
+import copy
+from math import pi, tau, dist, fabs, cos
+
+import rospy
+# import moveit_msgs.msg
+# import geometry_msgs.msg
+from baxter_core_msgs.msg import EndEffectorCommand
+
+import moveit_commander
+from moveit_commander.conversions import *
+from planit import Planner
+from planit.utils import *
+
+import pybullet as p
+from robot import Robot
+
+
+class BaxterPlanner(Planner):
+
+    class MoveGroup(moveit_commander.MoveGroupCommander):
+
+        def __init__(
+            self,
+            name,
+            robot_description="robot_description",
+            ns="",
+            wait_for_servers=5.0,
+            rate=240
+        ):
+            super().__init__(name, robot_description, ns, wait_for_servers)
+            self.gripper_left_cmd = 'open'
+            self.gripper_right_cmd = 'open'
+            self.robot = None
+            self.rate = rospy.Rate(rate)
+
+        def execute(self, plan_msg, wait=True):
+            super().execute(plan_msg, wait)
+            joint_names = plan_msg.joint_trajectory.joint_names
+            indices = [self.robot.joint_name2ind[x] for x in joint_names]
+            print(joint_names)
+            print(indices)
+            points = plan_msg.joint_trajectory.points
+            p_secs = 0
+            for point in points[1:]:
+                secs = point.time_from_start.to_sec()
+                poss = point.positions
+                print(secs)
+                print(poss)
+                if secs == 0:
+                    continue
+                num_steps = int(240 * (secs - p_secs))
+                p_secs = secs
+                for i in range(num_steps):
+                    p.setJointMotorControlArray(
+                        bodyIndex=self.robot.robot_id,
+                        jointIndices=indices,
+                        controlMode=p.POSITION_CONTROL,
+                        targetPositions=poss,
+                        physicsClientId=self.robot.pybullet_id
+                    )
+                    self.robot.set_gripper('left', state=self.gripper_left_cmd)
+                    self.robot.set_gripper('right', state=self.gripper_right_cmd)
+                    # print(self.gripper_left_cmd)
+                    # print(self.gripper_right_cmd)
+                    p.stepSimulation()
+                    self.rate.sleep()
+
+    def __init__(
+        self, robot, is_sim=False, commander_args=['joint_states:=/robot/joint_states']
+    ):
+
+        self.pb_robot = robot
+        gripper_width = robot.right_flim[1] * 2  # gripper width
+        super().__init__(gripper_width, is_sim, commander_args)
+        self.move_group_left = self.MoveGroup('left_arm')
+        self.move_group_left.robot = self.pb_robot
+        self.move_group_right = self.MoveGroup('right_arm')
+        self.move_group_right.robot = self.pb_robot
+
+        if not is_sim:
+            self.eef_pub_left = rospy.Publisher(
+                '/robot/end_effector/left_gripper/command',
+                EndEffectorCommand,
+                queue_size=5,
+            )
+            self.eef_pub_right = rospy.Publisher(
+                '/robot/end_effector/right_gripper/command',
+                EndEffectorCommand,
+                queue_size=5,
+            )
+
+    def do_end_effector(self, command, group_name="left_hand", move_group=None):
+        if self.is_sim:
+            if move_group is not None:
+                if group_name == "left_hand":
+                    move_group.gripper_left_cmd = command
+                if group_name == "right_hand":
+                    move_group.gripper_right_cmd = command
+
+            # self.pb_robot.set_gripper(group_name.split('_')[0], state=command)
+
+            move_group = self.MoveGroup(group_name)
+            move_group.robot = self.pb_robot
+            joint_goal = move_group.get_current_joint_values()
+
+            if command == 'open':
+                joint_goal = [0.02, -0.02]
+            elif command == 'close':
+                joint_goal = [0.018, -0.018]
+
+            move_group.go(joint_goal, wait=True)
+            move_group.stop()
+        else:
+            eef_cmd = EndEffectorCommand()
+            eef_cmd.id = 65664
+            if command == 'open':
+                eef_cmd.command = 'go'
+                eef_cmd.args = '{\"position\":100.0, \"force\":100}'
+            elif command == 'close':
+                eef_cmd.command = 'go'
+                eef_cmd.args = '{\"position\":0.0, \"force\":100}'
+            if group_name == 'left_hand':
+                self.eef_pub_left.publish(eef_cmd)
+            elif group_name == 'right_hand':
+                self.eef_pub_right.publish(eef_cmd)
+            else:
+                print("Invalid group name!", file=sys.stderr)
+
+    def pick(
+        self,
+        obj_name,
+        constraints=None,
+        pre_disp_dist=0.05,
+        post_disp_dir=(0, 0, 1),
+        post_disp_dist=0.05,
+        eef_step=0.005,
+        jump_threshold=0.0,
+        v_scale=0.25,
+        a_scale=1.0,
+        grasping_group="left_hand",
+        group_name="left_arm",
+    ):
+        move_group = self.MoveGroup(group_name)
+        move_group.robot = self.pb_robot
+        move_group.set_num_planning_attempts(25)
+        move_group.set_planning_time(10.0)
+        if constraints is not None:
+            move_group.set_path_constraints(constraints)
+        # move_group.set_support_surface_name(support)
+
+        # open gripper
+        self.do_end_effector('open', group_name=grasping_group, move_group=move_group)
+
+        # plan to pre goal poses
+        poses = self.grasps.get_simple_grasps(obj_name, (0, 0, -pre_disp_dist))
+        move_group.set_pose_targets(poses)
+        success, raw_plan, planning_time, error_code = move_group.plan()
+        move_group.clear_pose_targets()
+        if not success:
+            return error_code
+        else:
+            print("Planned pick for", obj_name, "in", planning_time, "seconds.")
+
+        # retime and execute trajectory
+        plan = move_group.retime_trajectory(
+            self.robot.get_current_state(),
+            raw_plan,
+            velocity_scaling_factor=v_scale,
+            acceleration_scaling_factor=a_scale,
+        )
+        move_group.execute(plan, wait=True)
+        move_group.stop()
+
+        # slide to goal
+        cpose = pose_msg2homogeneous(move_group.get_current_pose().pose)
+        trans = translation_matrix((0, 0, pre_disp_dist))
+        wpose = homogeneous2pose_msg(concatenate_matrices(cpose, trans))
+        waypoints = [copy.deepcopy(wpose)]
+        raw_plan, fraction = move_group.compute_cartesian_path(
+            waypoints, eef_step, jump_threshold, avoid_collisions=False
+        )
+        print("Planned approach", fraction * pre_disp_dist, "for", obj_name, ".")
+        if fraction < 0.5:
+            return -fraction
+
+        # retime and execute trajectory
+        plan = move_group.retime_trajectory(
+            self.robot.get_current_state(),
+            raw_plan,
+            velocity_scaling_factor=v_scale,
+            acceleration_scaling_factor=a_scale,
+        )
+        move_group.execute(plan, wait=True)
+        move_group.stop()
+        print("Approached", obj_name, ".")
+
+        # close gripper
+        self.do_end_effector('close', group_name=grasping_group, move_group=move_group)
+
+        # attach to robot chain
+        success = self.attach(
+            obj_name, grasping_group=grasping_group, group_name=group_name
+        )
+        if success:
+            print("Picked", obj_name, ".")
+        else:
+            return -1
+
+        # displace
+        scale = post_disp_dist / dist(post_disp_dir, (0, 0, 0))
+        wpose = move_group.get_current_pose().pose
+        wpose.position.x += scale * post_disp_dir[0]
+        wpose.position.y += scale * post_disp_dir[1]
+        wpose.position.z += scale * post_disp_dir[2]
+        waypoints = [copy.deepcopy(wpose)]
+        raw_plan, fraction = move_group.compute_cartesian_path(
+            waypoints, eef_step, jump_threshold, avoid_collisions=True
+        )
+        print("Planned displacement", fraction * post_disp_dist, "for", obj_name, ".")
+        if fraction < 0.5:
+            return -fraction
+
+        # retime and execute trajectory
+        plan = move_group.retime_trajectory(
+            self.robot.get_current_state(),
+            raw_plan,
+            velocity_scaling_factor=v_scale,
+            acceleration_scaling_factor=a_scale,
+        )
+        move_group.execute(plan, wait=True)
+        move_group.stop()
+        print("Displaced", obj_name, ".")
+        return True
